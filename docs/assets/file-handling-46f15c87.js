7037a189c4878c9bd18d14a540e13541fd28580e
Thomas Steiner <tomac@google.com>
2023-11-14 12:29:22 +0100

    Add explanation

---
+++ docs/assets/file-handling-46f15c87.js
@@ -0,0 +1,1 @@
+import{e as t,o as a,c as n,a as c}from"./index-e792ca54.js";launchQueue.setConsumer(async i=>{if(!i.files.length)return;const s=[];for(const o of i.files){const e=await o.getFile();e.handle=o,!(e.type&&e.type!=="application/wasm"||!e.name.endsWith(".wasm"))&&s.push(e)}t&&a.checked&&await n(s),c(s)});
