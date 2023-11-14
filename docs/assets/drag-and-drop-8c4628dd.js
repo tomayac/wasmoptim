f769a063b9f0ce53d965388fd07b69bfc0501539
Thomas Steiner <tomac@google.com>
2023-11-14 19:35:20 +0100

    Center layout

---
+++ docs/assets/drag-and-drop-8c4628dd.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-da01a0b5.js";e.parentNode.hidden=!1;e.addEventListener("change",()=>{localStorage.setItem("overwrite",e.checked)});localStorage.getItem("overwrite")!=="true"?e.checked=!1:e.checked=!0;
